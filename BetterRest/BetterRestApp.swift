3f741ab793d86089bd278b750c5f7724298ddb68
Vaibhav A <whybhavwastaken@gmail.com>
2022-06-05 23:50:34 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by whybhav on 05/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
